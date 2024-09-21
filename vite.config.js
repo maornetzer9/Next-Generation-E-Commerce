1c27fdf9575c3571b62a77960193338e2863910b
maornetzer9 <maorgithub@gmail.com>
2024-09-21 15:10:36 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    minify: 'esbuild', // Use esbuild for faster minification
+    chunkSizeWarningLimit: 512, // Adjust this as needed
+  },
 })
